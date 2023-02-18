26390557dbc996cd88baced4af4dbdca89113116
Namheon Baek <namhundred@naver.com>
2023-02-18 16:53:49 +0900

    feat: add Switch component
    Switch 컴포넌트 추가

--- src/components/Swap/Calculator.tsx
+++ src/components/Swap/Calculator.tsx
@@ -1,11 +1,12 @@
 import { Lower } from "./Lower";
 import { Upper } from "./Upper";
-
+import { Switch } from "./Switch";
 export function Calculator() {
   return (
     <div className="swap-body">
       <Upper />
       <Lower />
+      <Switch />
     </div>
   );
 }
---
+++ src/components/Swap/Switch.tsx
@@ -0,0 +1,7 @@
+export function Switch() {
+  return (
+    <button className="swap-switch" onClick={() => alert("준비 중입니다.")}>
+      ↓
+    </button>
+  );
+}
