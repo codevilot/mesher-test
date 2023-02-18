b95b60df25272f9dfe37f9b5402fa431bc1f77bf
Namheon Baek <namhundred@naver.com>
2023-02-19 00:51:14 +0900

    feat: add copy style.css
    빌드할 때 style.css 복사하는 기능 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
 const __dirname = path.resolve();
@@ -27,9 +28,23 @@ export default (env, argv) => {
           test: /\.tsx?$/,
           use: ["babel-loader", "ts-loader"],
         },
+        {
+          test: /\.s?css$/, // .css 또는 scss로 끝나는 파일 인식
+          use: [
+            "style-loader", // html에 삽입해주는 역할
+            "css-loader", // 먼저 해석됨. js에서 css에서 인식하도록 해석
+            "postcss-loader", // 공급업체 접두사 적용
+            "sass-loader", // 가장 먼저 해석됨. js에서 scss에서 인식하도록 해석
+          ],
+        },
       ],
     },
     plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "public/style.css" }, // 번들링 결과물에 스태틱 파일을 포함시킴
+        ],
+      }),
       new webpack.ProvidePlugin({
         React: "react",
       }),
